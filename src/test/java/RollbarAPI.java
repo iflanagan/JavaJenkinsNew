bae094b631ce211e128f474b2c9dd51cfbbc8857
Ian Flanagan <ianianf@yahoo.com>
2021-03-24 09:01:44 -0500

    added resolve script file.

--- src/test/java/RollbarAPI.java
+++ src/test/java/RollbarAPI.java
@@ -117,6 +117,78 @@ public class RollbarAPI {
         return value;
     }
 
+    public static void getOccurrences(String token, String env, String vers) {
+
+      System.out.println("Calling getOccurrences() method now.");
+
+       try {
+
+           // allow multiple TLS versions
+           System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
+
+           // create the HttpURLConnection
+           url = new URL(deployURL);
+           HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+           connection.addRequestProperty("X-Rollbar-Access-Token", token);
+           connection.addRequestProperty("environment", env);
+           connection.addRequestProperty("revision", vers);
+           connection.addRequestProperty("rollbar_username", "ianianf");
+           connection.addRequestProperty("local_username", "iflanagan");
+           connection.addRequestProperty("comment", "Deployment from Java REST code");
+           connection.addRequestProperty("Content-Type", "application/json; charset=utf-8");
+           connection.addRequestProperty("Content-Length", "1000");
+           // connection.setRequestProperty("Content-Length", "1000"); // Content-Length:0
+
+
+           // connection.setRequestProperty("Content-Length", "20");
+           // just want to do an HTTP GET here
+           connection.setRequestMethod("POST");
+
+           // uncomment this if you want to write output to this url
+           connection.setDoOutput(true);
+
+           // give it 15 seconds to respond
+           connection.setReadTimeout(15 * 1000);
+           connection.connect();
+
+           int responseCode = connection.getResponseCode();
+           System.out.println("Code is: " +responseCode);
+           //    System.out.println("Rollbar Deploy response is : " +connection.getResponseMessage());
+
+           if (responseCode == 200) {
+               System.out.println("Deploy was successful");
+               value = true;
+           } else {
+               System.out.println("Deploy was unsuccessful");
+           }
+           // read the output from the server
+           reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.defaultCharset()));
+           stringBuilder = new StringBuilder();
+
+           while ((line = reader.readLine()) != null) {
+               stringBuilder.append(line + "\n");
+           }
+
+           String resp = stringBuilder.toString();
+           System.out.println("\nRollbar API response is: from deploy is: " +resp);
+
+        } catch (Exception e ) {
+           System.out.println("Can't Call getOccurrences() method now." +e.getMessage());
+            e.printStackTrace();
+        } finally {
+
+           // close the reader; this can throw an exception too, so
+           // wrap it in another try/catch block.
+           if (reader != null) {
+               try {
+                   reader.close();
+               } catch (IOException io) {
+                   io.printStackTrace();
+               }
+           }
+       }
+    }
+
     public boolean deploy(String token, String env, String vers) {
 
         URL url = null;
